8fab99f991c2ce4228b2e4bab746f05c72312498
Gökhan Alıcı <gokhanaliccii@gmail.com>
2017-12-27 00:43:45 +0200

    isEmpty function created for arrays

--- app/src/main/java/com/gokhanaliccii/flavorhunter/util/CollectionUtil.java
+++ app/src/main/java/com/gokhanaliccii/flavorhunter/util/CollectionUtil.java
@@ -16,4 +16,9 @@ public class CollectionUtil {
         return !isEmpty(list);
     }
 
+    public static <T> boolean isEmpty(T[] array) {
+        return array == null || array.length == 0;
+    }
+
+
 }
