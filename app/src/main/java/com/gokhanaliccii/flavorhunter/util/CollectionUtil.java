585bf756f32581eb08eed01d1d648c4d0d55208a
Gökhan Alıcı <gokhanaliccii@gmail.com>
2017-12-27 00:42:12 +0200

    Collection created for common collection functions

---
+++ app/src/main/java/com/gokhanaliccii/flavorhunter/util/CollectionUtil.java
@@ -0,0 +1,19 @@
+package com.gokhanaliccii.flavorhunter.util;
+
+import java.util.List;
+
+/**
+ * Created by gokhan on 27/12/17.
+ */
+
+public class CollectionUtil {
+
+    public static <T> boolean isEmpty(List<T> list) {
+        return list == null || list.size() == 0;
+    }
+
+    public static <T> boolean hasItem(List<T> list) {
+        return !isEmpty(list);
+    }
+
+}
